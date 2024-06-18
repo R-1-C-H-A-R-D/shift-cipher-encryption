d8fd1a2e83c4c3417ae1e5876ed55a9302847550
R1CH13 <4rnold.richard@gmail.com>
2024-06-18 22:50:22 +0300

    shift cipher

---
+++ shift cipher.c
@@ -0,0 +1,43 @@
+// A SHIFT CIPHER ENCRYPTION CODE.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+void encrypt(char *plaintext, int key) {
+
+    char ch;
+    int i = 0;
+
+    while (plaintext[i] != '\0') {
+
+        ch = plaintext[i];
+
+        if(isupper(ch)) {
+            ch = (ch + key - 'A') % 26 + 'A';
+        }
+
+        else if (islower(ch)) {
+            ch = (ch + key - 'a') % 26 + 'a';
+        }
+
+        plaintext[i] = ch;
+        i++;
+    }
+}
+
+int main(void) {
+    char plaintext[1000];
+    printf("Enter your plantext: ");
+    scanf(" %[^\n]s", plaintext);
+
+    int key;
+    printf("Enter number of shifts: ");
+    scanf("%d", &key);
+
+    encrypt(plaintext, key);
+    printf("Enrypted test is: %s\n", plaintext);
+
+    return EXIT_SUCCESS;
+}
